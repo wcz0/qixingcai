bd33dcd9f890f31a04d362927da804ef685e473b
wcz0 <wcz0@outlook.com>
2021-10-31 15:41:22 +0800

    add files

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class IndexController extends Controller
+{
+    public function index(Request $request)
+    {
+        // 抓取七星彩官网数据
+        $response = Http::get('https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=04&provinceId=0&pageSize=30&isVerify=1&pageNo=1');
+
+        return dump($response->json());
+        // 写入数据库
+    }
+}
